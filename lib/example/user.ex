32fe052a0c7a057ac4f300771905e0e666489a29
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-11-04 18:46:48 +0900

    day7 シナリオで使用するプロジェクト~スキーマの作成まで

---
+++ lib/example/user.ex
@@ -0,0 +1,11 @@
+defmodule Example.User do
+  use Ecto.Schema
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :age, :integer
+
+    timestamps()
+  end
+end
