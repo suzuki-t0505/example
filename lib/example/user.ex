9eda0b4719216a76be438477aea64467cd854f2c
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-11-25 11:29:05 +0900

    day10

--- lib/example/user.ex
+++ lib/example/user.ex
@@ -15,6 +15,7 @@ defmodule Example.User do
     user
     |> cast(params, [:first_name, :last_name, :age, :email])
     |> validate_required([:first_name, :last_name])
+    |> validate_last_name()
     |> validate_email()
 
   end
@@ -27,10 +28,13 @@ defmodule Example.User do
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "Must include the @ symbol, do not include spaces.")
   end
 
-  def sample_changeset(user, params \\ %{}) do
-    user
-    |> cast(params, [:first_name, :last_name, :age])
-    |> validate_required(:first_name, message: "Please enter your first name.")
-    |> validate_required(:last_name, message: "Please enter your last name.")
+  def validate_last_name(cs) do
+    last_name = get_field(cs, :last_name)
+
+    if last_name in ["Sato", "Suzuki", "Takahashi", "Tanaka", "Ito"] do
+      cs
+    else
+      add_error(cs, :last_name, "Please use the last name specified.")
+    end
   end
 end
